81ae169bcc1186296ede22ad05a144e61bc9bd40
singcl <24661881@qq.com>
2018-05-01 23:53:54 +0800

    :art: Thunk functional with ECMA

---
+++ example/thunk.exam.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+var path = require('path');
+
+// 正常版本的readFile（多参数版本）
+// fs.readFile(fileName, callback);
+
+// Thunk-es5
++function () {
+    var Thunk = require('../thunk-es5');
+
+    var filePath = path.resolve(__dirname, './test.txt');
+    var callback = function (err, data) {
+        if (err) throw err;
+        console.log(data);
+    };
+
+    // Thunk版本的readFile（单参数版本）
+    var readFileThunk = Thunk(fs.readFile);
+    readFileThunk(filePath, 'utf8')(callback);
+}();
+
+// Thunk-es6
+!function () {
+    var Thunk = require('../thunk-es6')
+
+    var filePath = path.resolve(__dirname, './test.txt')
+    var callback = function (err, data) {
+        if (err) throw err
+        console.log(data)
+    }
+
+    // Thunk版本的readFile（单参数版本）
+    var readFileThunk = Thunk(fs.readFile)
+    readFileThunk(filePath, 'utf8')(callback)
+}();
+
+// Thunk
+(function () {
+    var Thunk = require('../thunk')
+
+    var filePath = path.resolve(__dirname, './test.txt')
+    var callback = function (err, data) {
+        if (err) throw err
+        console.log(data)
+    }
+
+    // Thunk版本的readFile（单参数版本）
+    var readFileThunk = Thunk(fs.readFile)
+    readFileThunk(filePath, 'utf8')(callback)
+})();
---
+++ thunk.js
@@ -0,0 +1,18 @@
+/**
+ * JavaScript 语言是传值调用，它的 Thunk 函数含义有所不同。
+ * 在 JavaScript 语言中，Thunk 函数替换的不是表达式，而是多参数函数，
+ * 将其替换成一个只接受回调函数作为参数的单参数函数。
+ * 任何函数，只要参数有回调函数，就能写成 Thunk 函数的形式。下面是一个简单的ES6 Thunk 函数转换器
+ * 
+ * Thunk 函数有什么用？回答是以前确实没什么用，
+ * 但是 ES6 有了 Generator 函数，Thunk 函数现在可以用于 Generator 函数的自动流程管理。
+ * 
+ * @see http://es6.ruanyifeng.com/?search=x&x=0&y=0#docs/generator-async
+ * @param {Function} fn 需要Thunk的函数
+ */
+var Thunk = (fn) => (...args) => (callback) => fn.call(this, ...args, callback)
+
+/**
+ * @module Thunk
+ */
+module.exports = Thunk;
