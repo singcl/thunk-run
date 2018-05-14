5fd22e3a9484aa70bce272d153188e6c3c7cc31b
singcl <24661881@qq.com>
2018-05-14 10:01:20 +0800

    :art: optimize promisify function, If the args.length > 3 then return a array data

--- src/promisify.js
+++ src/promisify.js
@@ -10,9 +10,19 @@ function promisify(fn) {
         var args = Array.prototype.slice.call(arguments);
 
         return new Promise(function (resolve, reject) {
-            args.push(function (err, data) {
+            // 增加callback 函数参数大于等于3个兼容
+            // 当参数大于等于3个时 resolve 的data 是一个数组
+            args.push(function () {
+                var args2 = Array.prototype.slice.call(arguments);
+                var err = args2[0];
                 if (err) return reject(err);
-                resolve(data);
+
+                var data = args2.slice(1);
+                if (data.length > 1) {
+                    resolve(data);
+                } else {
+                    resolve(data[0]);
+                }
             });
             fn.apply(ctx, args);
         });
