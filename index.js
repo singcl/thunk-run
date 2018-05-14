cad6bec3e80a46745992cfe31fe53cb0d0158d73
singcl <24661881@qq.com>
2018-05-14 15:51:38 +0800

    :art: update the promisify.ts

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var thunkify = require('./src/thunkify');
-var promisify = require('./src/promisify');
+// var thunkify = require('./src/thunkify');
+// var promisify = require('./src/promisify');
 
-var thunkifyRun = require('./src/thunkifyRun');
-var promisifyRun = require('./src/promisifyRun');
+// var thunkifyRun = require('./src/thunkifyRun');
+// var promisifyRun = require('./src/promisifyRun');
 
-module.exports = {
-    thunkify: thunkify,
-    promisify: promisify,
-    thunkifyRun: thunkifyRun,
-    promisifyRun: promisifyRun
-};
+// module.exports = {
+//     thunkify: thunkify,
+//     promisify: promisify,
+//     thunkifyRun: thunkifyRun,
+//     promisifyRun: promisifyRun
+// };
