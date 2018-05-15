366484a15525fd715509366aa21d2591089edf8c
singcl <24661881@qq.com>
2018-05-15 10:34:10 +0800

    :art: upgrade the project to typescript.

---
+++ dist/index.m.js
@@ -0,0 +1,2 @@
+var r=require("./src/promisify"),i=require("./src/promisifyRun"),u=require("./src/thunkify"),e=require("./src/thunkifyRun");module.exports={promisify:r,promisifyRun:i,thunkify:u,thunkifyRun:e};
+//# sourceMappingURL=index.m.js.map
